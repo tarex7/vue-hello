17b324a27e452e89060fa29ce391028c00ec7799
tarex7 <tarex7@gmail.com>
2022-07-11 14:30:08 +0200

    Done

---
+++ script.js
@@ -0,0 +1,9 @@
+const root = new Vue({
+  el: "#root",
+  data: {
+    title: "Hello Vue!",
+    subtitle: "I used v-text!",
+    imageSrc: "https://source.unsplash.com/random/500x500/?nature",
+    alt: "nature random image",
+  },
+});
